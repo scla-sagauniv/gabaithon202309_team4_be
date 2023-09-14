137f2ac290db7f0fb7742055147bf2c9d09bdcee
nnnnn81 <nnnnnagi81@gmail.com>
2023-09-14 16:28:10 +0900

    update: テーブルの作成と初期化

--- db/table.py
+++ db/table.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # 接続情報の構成と接続
 host = os.environ['SERVER_NAME']
-dbname = ""
+dbname = "contents"
 user = os.environ['ADMIN_USERNAME']
 password = os.environ['ADMIN_PASSWORD']
 sslmode = "require"
@@ -18,10 +18,57 @@ conn = psycopg2.connect(conn_string)
 print("Connection established")
 cursor = conn.cursor()
 
+cursor.execute("DROP TABLE IF EXISTS opinions;")
+print("Finished dropping table (if existed)")
 # テーブル作成
 cursor.execute(
-    "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
-print("Finished creating table")
+    "CREATE TABLE opinions (id serial PRIMARY KEY, genre VARCHAR(50), content VARCHAR(50), status INTEGER);")
+print("Finished creating contents table")
+
+cursor.execute(
+    "INSERT INTO opinions (genre, content) VALUES (%s, %s);", ("other", "hogehoge"))
+
+cursor.execute("SELECT * FROM opinions;")
+rows = cursor.fetchall()
+
+for row in rows:
+    print("Data row = (%s, %s, %s, %s)" %
+          (str(row[0]), str(row[1]), str(row[2]), str(row[3])))
+# 保存・終了
+conn.commit()
+cursor.close()
+conn.close()
+
+
+host = os.environ['SERVER_NAME']
+dbname = "users"
+user = os.environ['ADMIN_USERNAME']
+password = os.environ['ADMIN_PASSWORD']
+sslmode = "require"
+
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(
+    host, user, dbname, password, sslmode)
+conn = psycopg2.connect(conn_string)
+print("Connection established")
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS users;")
+print("Finished dropping table (if existed)")
+
+# テーブル作成
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, username VARCHAR(50), email VARCHAR(50), password VARCHAR(50), status INTEGER);")
+print("Finished creating users table")
+
+cursor.execute(
+    "INSERT INTO users (username, email, password) VALUES (%s, %s, %s);", ("admin", "admin@gabaithon.com", "admin"))
+
+cursor.execute("SELECT * FROM users;")
+rows = cursor.fetchall()
+
+for row in rows:
+    print("Data row = (%s, %s, %s, %s)" %
+          (str(row[0]), str(row[1]), str(row[2]), str(row[3])))
 
 # 保存・終了
 conn.commit()
